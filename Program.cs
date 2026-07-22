1ac80d5f246c70895534e9f7433f3df4c721952c
Thiago Godoy Cunha <109763990+Thiago-Godoy-Cunha@users.noreply.github.com>
2026-07-22 19:29:45 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,3 @@
 ﻿using RPG_Assistant.Cli;
-using RPG_Assistant.Models;
-Item item = Item.Create("Arco Curto");
-Console.WriteLine(item.Name + "\n" + item.Description);
-Item item2 = Item.Create("Flechas");
-Console.WriteLine(item2.Name + "\n" + item2.Description);
+
 CharacterCreationFlow.Run();
